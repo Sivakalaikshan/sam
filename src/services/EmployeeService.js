3243ecbdb3d304a7bc6bba1be2b06c69c47b3e2a
Sivakalaikshan <115426224+Sivakalaikshan@users.noreply.github.com>
2022-10-10 01:20:35 +0530

    Delete src directory

--- src/services/EmployeeService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const EMPLOYEE_LIST_TABLE_URL='http://localhost:8080/getAll';
-class EmployeeSerivce{
-    listAll(){
-        return axios.get(EMPLOYEE_LIST_TABLE_URL)
-    }
-}
-export default new EmployeeSerivce();
